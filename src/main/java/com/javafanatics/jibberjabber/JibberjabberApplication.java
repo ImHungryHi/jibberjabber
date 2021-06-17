a6bdf11275474eeb4922152fbe89171fce126e30
ChrisDS <chris.de.smedt.91@gmail.com>
2021-06-17 11:02:31 +0200

    initial commit

---
+++ src/main/java/com/javafanatics/jibberjabber/JibberjabberApplication.java
@@ -0,0 +1,10 @@
+package com.javafanatics.jibberjabber;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibberjabberApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JibberjabberApplication.class, args);
+	}
+}
