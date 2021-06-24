a60d30396dec008a5abe9b4101fc29d832258ea3
ChrisDS <chris.de.smedt.91@gmail.com>
2021-06-24 13:14:00 +0200

    Experimenting with login & custom form info

--- src/main/java/com/javafanatics/jibberjabber/account/AuthenticationController.java
+++ src/main/java/com/javafanatics/jibberjabber/account/AuthenticationController.java
@@ -1,33 +1,48 @@
 package com.javafanatics.jibberjabber.account;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @Controller
 public class AuthenticationController {
+    AuthenticationManager authManager;
+
+    @Autowired
+    public void setAuthManager(AuthenticationManager authManager) {
+        this.authManager = authManager;
+    }
+
+    // Let login handle the "/" mapping, with redirect if logged in
     @GetMapping("/login")
     public String showLogin() {
         return "account/login";
     }
 
     @PostMapping("/login")
-    public String doLogin(@ModelAttribute User user, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
+    public String doLogin(@RequestBody MultiValueMap<String, String> formData, HttpServletRequest request) {
+    //public String doLogin(@ModelAttribute User user, BindingResult bindingResult) {
+        /*if (bindingResult.hasErrors()) {
             return "/login";
-        }
+        }*/
 
+        String user = formData.getFirst("email");
+        String pass = formData.getFirst("password");
 
-
-        /*UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(user, pass);
-        Authentication auth = authManager.authenticate(authReq);
-        SecurityContext sc = SecurityContextHolder.getContext();
-        sc.setAuthentication(auth);*/
+        authWithAuthManager(request, user, pass);
 
         return "redirect:/home";
     }
@@ -35,5 +50,24 @@ public class AuthenticationController {
     @GetMapping("/register")
     public String showRegister() {
         return "account/register";
+    }
+
+    // Google a way to look for extra fields that don't occur in entities
+    @PostMapping("/register")
+    public String doRegister(@ModelAttribute User user, BindingResult bindingResult) {
+        return null;
+    }
+
+    @GetMapping("/logout")
+    public String doLogout() {
+        // Logout
+        return "redirect:/login";
+    }
+
+    public void authWithAuthManager(HttpServletRequest request, String username, String password) {
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+        authToken.setDetails(new WebAuthenticationDetails(request));
+        Authentication authentication = authManager.authenticate(authToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
--- src/main/java/com/javafanatics/jibberjabber/config/SecurityConfig.java
+++ src/main/java/com/javafanatics/jibberjabber/config/SecurityConfig.java
@@ -1,18 +1,46 @@
 package com.javafanatics.jibberjabber.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private DataSource dataSource;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/register").permitAll()
+                .antMatchers("/register", "/css/**", "/img/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login").permitAll();
+                .loginPage("/login").permitAll()
+                .and().logout().permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authManager) throws Exception {
+        authManager.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT email, password, enabled FROM users WHERE email = ?")
+                .authoritiesByUsernameQuery("SELECT email, role FROM users WHERE u.email = ?");
     }
 }
