5d97f4bf1f1b09c6b006ea38a10ed7ed80d529d3
ChrisDS <chris.de.smedt.91@gmail.com>
2021-07-31 17:34:17 +0200

    Added jibbering (new jibber)

--- src/main/java/com/javafanatics/jibberjabber/message/JibberController.java
+++ src/main/java/com/javafanatics/jibberjabber/message/JibberController.java
@@ -1,13 +1,18 @@
 package com.javafanatics.jibberjabber.message;
 import com.javafanatics.jibberjabber.Tools;
+import com.javafanatics.jibberjabber.account.User;
 import com.javafanatics.jibberjabber.account.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import java.security.Principal;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -37,6 +42,7 @@ public class JibberController {
         model.addAttribute("handle", handle);
         model.addAttribute("jibbers", jibbers);
         model.addAttribute("tools", new Tools());
+        model.addAttribute("jabber", new Jibber());
 
         return "home";
     }
@@ -50,5 +56,20 @@ public class JibberController {
         model.addAttribute("tools", new Tools());
 
         return "home";
+    }
+
+    @PostMapping("/jibber")
+    public String doHomeJibber(@ModelAttribute Jibber jabber, BindingResult bindingResult, Principal principal) {
+        User currentUser = userService.getByHandle(principal.getName());
+
+        jabber.setCreatedDate(new Date());
+        jabber.setUser(currentUser);
+        jibberService.save(jabber);
+        return "redirect:/";
+    }
+
+    @PostMapping("/{id}/jibber")
+    public String doWallJibber(@PathVariable int id, @ModelAttribute Jibber jibber, Authentication authentication) {
+        return "redirect:/";
     }
 }
